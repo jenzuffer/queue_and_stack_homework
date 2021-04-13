e39130cb3566f200fe817cf6eec29e180bc9b3ca
Christian Moustesgård <cph-cm267@cphbusiness.dk>
2021-04-13 18:03:34 +0200

    hurtig implementeringer

---
+++ src/main/java/Stack.java
@@ -0,0 +1,37 @@
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class Stack<Item> implements Iterable<Item>{
+
+
+    public Stack(){}
+
+    void push(Item item){
+
+    }
+
+    public Item pop(){
+        return null;
+    }
+
+    public boolean IsEmpty(){
+        return false;
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    public void forEach(Consumer<? super Item> action) {
+
+    }
+
+    public Spliterator<Item> spliterator() {
+        return null;
+    }
+}
