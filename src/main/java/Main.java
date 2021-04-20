307e38ac5ee615e294ae7989b25ae540809ba22f
Tiba666 <frehr@hotmail.com>
2021-04-20 12:42:20 +0200

    added graftest

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,9 +22,34 @@ public class Main {
             System.out.println(dequeue);
         }
                  */
-        EdgeWeightedGraph edgeWeightedGraph = new EdgeWeightedGraph(2);
-        Edge e = new EdgeImpl(0 , 1 ,1);
+        EdgeWeightedGraph edgeWeightedGraph = new EdgeWeightedGraph(7);
+
+        Edge e = new EdgeImpl(0, 1, 1);
+        Edge e2 = new EdgeImpl(1, 2, 2);
+        Edge e3 = new EdgeImpl(1, 5, 3);
+        Edge e4 = new EdgeImpl(2, 5, 4);
+        Edge e5 = new EdgeImpl(1, 4, 5);
+        Edge e6 = new EdgeImpl(4, 5, 6);
+        Edge e7 = new EdgeImpl(5, 6, 7);
+        Edge e8 = new EdgeImpl(4, 6, 8);
+        Edge e9 = new EdgeImpl(3, 6, 9);
+        Edge e10 = new EdgeImpl(3, 4, 10);
+        Edge e11 = new EdgeImpl(0, 4, 11);
+        Edge e12 = new EdgeImpl(0, 3, 12);
+
         edgeWeightedGraph.addEdge(e);
+        edgeWeightedGraph.addEdge(e2);
+        edgeWeightedGraph.addEdge(e3);
+        edgeWeightedGraph.addEdge(e4);
+        edgeWeightedGraph.addEdge(e5);
+        edgeWeightedGraph.addEdge(e6);
+        edgeWeightedGraph.addEdge(e7);
+        edgeWeightedGraph.addEdge(e8);
+        edgeWeightedGraph.addEdge(e9);
+        edgeWeightedGraph.addEdge(e10);
+        edgeWeightedGraph.addEdge(e11);
+        edgeWeightedGraph.addEdge(e12);
+
         LazyPrimMst lazyPrimMst = new LazyPrimMst(edgeWeightedGraph);
         for (Edge edge : lazyPrimMst.edges()) {
             int either = edge.either();
